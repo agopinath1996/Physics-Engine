565c4983ea89210a4d0beb16f38a58cc506c392a
Arjun Raja <ar6841@nyu.edu>
2024-01-28 00:53:05 -0600

    Add CSOs

---
+++ CSO.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "RigidBody.h"
+#include "CommonMathTypes.h"
+
+namespace CSO{
+    
+    //Functiin to dertermine if the CSO of two shapes contains the origin
+    template <typename T>
+    void CSOSupport(const Body::Collider<T> &colliderA,
+                    const Body::Collider<T> &colliderB,
+                    const MathCommon::Vector3<T> &dir, 
+                    MathCommon::Vector3<T> &support, 
+                    MathCommon::Vector3<T> &supportA, 
+                    MathCommon::Vector3<T> &supportB)
+    {
+        const Body::RigidBody<T> *bodyA = colliderA.Surface();
+        const Body::RigidBody<T> *bodyB = colliderB.Surface();
+
+        // convert search direction to model space
+        const MathCommon::Vector3<T> localDirA = bodyA->GlobalToLocalVec(dir);
+        const MathCommon::Vector3<T> localDirB = bodyB->GlobalToLocalVec(-dir);
+
+        // compute support points in model space
+        supportA = colliderA.Support(localDirA);
+        supportB = colliderB.Support(localDirB);
+
+        // convert support points to world space
+        supportA = bodyA->LocalToGlobal(supportA);
+        supportB = bodyB->LocalToGlobal(supportB);
+
+        // compute CSO support point
+        support = supportA - supportB; 
+    }
+}
